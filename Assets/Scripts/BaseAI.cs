8a499d614e2f90687eeccb5985f1d60e92309415
zappymax <westmax@sbcglobal.net>
2016-11-18 12:46:42 -0600

    Created framework for basic AI script

---
+++ Assets/Scripts/BaseAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CheckStatus ()
+    {
+        //Use this to figure out if there needs to be any changes to the current
+        //status and update if need be
+    }
+}
