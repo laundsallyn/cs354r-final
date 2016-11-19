c13e0d0a5b6731295c5b318078d11379426dd581
zappymax <westmax@sbcglobal.net>
2016-11-18 18:19:50 -0600

    Added some basic AI

--- Assets/Scripts/BaseAI.cs
+++ Assets/Scripts/BaseAI.cs
@@ -3,15 +3,46 @@ using System.Collections;
 
 public class BaseAI : MonoBehaviour {
 
+    public float targetDistance;
+    public float lookDistance;
+    public float attackDistance;
+    public float movementSpeed;
+    public float damping;
+    public Transform target;
+    Rigidbody theRigidBody;
+    Renderer myRenderer;
+
 	// Use this for initialization
 	void Start () {
+        myRenderer = GetComponent<Renderer>();
+        theRigidBody = GetComponent<Rigidbody>();
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        targetDistance = Vector3.Distance(target.position, transform.position);
+        if(targetDistance<lookDistance)
+        {
+            myRenderer.material.color = Color.yellow;
+            facePlayer();
+        }
+        if(targetDistance<attackDistance)
+        {
+            attackTarget();
+        }
 	}
+
+    void facePlayer()
+    {
+        Quaternion newRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime*damping);
+    }
+
+    void attackTarget()
+    {
+        theRigidBody.AddForce(transform.forward * movementSpeed);
+    }
 
     void CheckStatus ()
     {
