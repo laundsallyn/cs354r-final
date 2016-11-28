7c87f375175d6af66b1db71214f7fabb7e8c4a24
Yipu Wang <yipu.wang@utexas.edu>
2016-11-28 12:17:28 -0600

    added main character, animations

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FPSController : MonoBehaviour {
     private Vector3 speed;
+    private float gravity = 3f;
+    private Vector3 jumpSpeed = Vector3.zero;
     private enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     private RotationAxes axes = RotationAxes.MouseXAndY;
     private float sensitivityX = 1F;
@@ -14,37 +16,96 @@ public class FPSController : MonoBehaviour {
     float rotationY = 0F;
     private Camera playerCam;
     private bool firstPerson = true;
+    private Animation ani, fpsAni;
+    public GameObject fpsArm;
+    public Canvas canvas;
     // Use this for initialization
     void Start () {
         speed = Vector3.zero;
         playerCam = GetComponentInChildren<Camera>();
+        ani = GetComponent<Animation>();
+        fpsAni = fpsArm.GetComponent<Animation>();
+        Debug.Log("Clip count is " + fpsAni.GetClipCount());
+        //Debug.Log(fpsAni.GetClip("fireWeapon"));
+        ani["walk"].wrapMode = WrapMode.Loop;
+        ani["jump"].speed = 7;
+        ani["Sidewalk"].speed = 2;
+        ani["Sidewalk"].wrapMode = WrapMode.Loop;
+        ani["Crouch"].speed = 3;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown("w"))
-            speed.z += 25;
-        if (Input.GetKeyUp("w"))
-            speed.z -= 25;
+        {
+            ani["walk"].speed = 5;
+            ani.Play("walk");
+            speed.z += 22;
+        }
+           
+        if (Input.GetKeyUp("w")) { 
+            ani.Stop("walk");
+            speed.z -= 22;
+        }
         if (Input.GetKeyDown("s"))
-            speed.z -= 25;
+        {
+            ani["walk"].speed = -5;
+            ani.Play("walk");
+            speed.z -= 20;
+        }
         if (Input.GetKeyUp("s"))
-            speed.z += 25;
+        {
+            ani.Stop("walk");
+            speed.z += 20;
+        }
+        
         if (Input.GetKeyDown("a"))
+        {
+            ani.Play("Sidewalk");
             speed.x -= 15;
+        }
         if (Input.GetKeyUp("a"))
+        {
+            ani.Stop("Sidewalk");
             speed.x += 15;
+        }
+
         if (Input.GetKeyDown("d"))
+        {
+            ani.Play("Sidewalk");
             speed.x += 15;
+        }
         if (Input.GetKeyUp("d"))
+        {
+            ani.Stop("Sidewalk");
             speed.x -= 15;
+        }
         transform.Translate(speed * Time.deltaTime,Space.Self);
         rotate();
+        if (Input.GetMouseButtonDown(0))
+        {
+            fpsAni.Play("fireWeapon");
+
+        }
 
         // buggy third person view
-        //if (Input.GetKeyDown("f"))
-        //    thirdPersonView();
+        if (Input.GetKeyDown("f"))
+            thirdPersonView();
+        if (Input.GetKeyDown("space"))
+        {
+            jumpSpeed.y = 0.6f;
+            ani.Play("jump");
+        }
+    }
 
+    void FixedUpdate()
+    {
+        if (transform.position.y == 0.0f && jumpSpeed.y < 0.1f) return;
+
+        jumpSpeed.y -= gravity * Time.fixedDeltaTime;
+        transform.Translate(jumpSpeed);
+        if (transform.position.y < 0.0f)
+            transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
     }
     private void thirdPersonView()
     {
@@ -52,34 +113,25 @@ public class FPSController : MonoBehaviour {
         {
             firstPerson = false;
             playerCam.transform.Translate(new Vector3(1, 0, -9),Space.Self);
+            fpsArm.SetActive(false);
         }
         else
         {
             playerCam.transform.Translate(new Vector3(1, 0, 9),Space.Self);
             firstPerson = true;
+            fpsArm.SetActive(true);
         }
     }
     private void rotate()
     {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
-            playerCam.transform.localEulerAngles = new Vector3(-rotationY,0, 0);
-            transform.localEulerAngles = new Vector3(0, rotationX, 0);
-        }
-        //else if (axes == RotationAxes.MouseX)
-        //{
-        //    transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        //}
-        //else
-        //{
-        //    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        //    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-        //    playerCam.transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        //}
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        playerCam.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+
     }
 }
