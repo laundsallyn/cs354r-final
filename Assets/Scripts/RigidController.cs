dc1aa26b39463d77f3cc64811348f2de9796bcb1
Peter Scamman <pscamman@gmail.com>
2016-11-28 20:27:53 -0600

    Latest networking with rigid bodies

--- Assets/Scripts/RigidController.cs
+++ Assets/Scripts/RigidController.cs
@@ -1,28 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-public class FPSController : NetworkBehaviour
+public class RigiedController : NetworkBehaviour
 {
-    private Vector3 force    = Vector3.zero;
-    private Vector3 curForce = Vector3.zero;
-    private float movForce     = 200;
-    private float maxVel       = 5;
+    private Vector3 speed = Vector3.zero;
+    private float maxSpeed = 25;
     private float sensitivityX = 1F;
     private float sensitivityY = 1F;
-    private float minimumY     = -60F;
-    private float maximumY     = 60F;
+    private float minimumY = -60F;
+    private float maximumY = 60F;
     float rotationY = 0F;
     private Camera mainCam;
     private Camera playerCam;
     private Rigidbody body;
-    private ConstantForce constForce;
     // Use this for initialization
     void Start()
     {
-        mainCam    = GameObject.Find("Main Camera").GetComponent<Camera>();
-        playerCam  = GetComponentInChildren<Camera>();
-        body       = GetComponentInChildren<Rigidbody>();
-        constForce = GetComponentInChildren<ConstantForce>();
+        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        playerCam = GetComponentInChildren<Camera>();
+        body = GetComponentInChildren<Rigidbody>();
         if (isLocalPlayer)
         {
             foreach (Renderer r in GetComponentsInChildren<Renderer>())
@@ -41,37 +37,31 @@ public class FPSController : NetworkBehaviour
         GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;
     }
 
-    void FixedUpdate()
-    {
-        if (!isLocalPlayer) return;
-
-        body.AddForce(curForce - (new Vector3(System.Math.Abs(body.velocity.x) * body.velocity.x, 0, System.Math.Abs(body.velocity.z) * body.velocity.z)));
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (!isLocalPlayer) return;
 
         if (Input.GetKeyDown("w"))
-            force.z += 1;
+            speed.z += 1;
         if (Input.GetKeyUp("w"))
-            force.z -= 1;
+            speed.z -= 1;
         if (Input.GetKeyDown("s"))
-            force.z -= 1;
+            speed.z -= 1;
         if (Input.GetKeyUp("s"))
-            force.z += 1;
+            speed.z += 1;
         if (Input.GetKeyDown("a"))
-            force.x -= 1;
+            speed.x -= 1;
         if (Input.GetKeyUp("a"))
-            force.x += 1;
+            speed.x += 1;
         if (Input.GetKeyDown("d"))
-            force.x += 1;
+            speed.x += 1;
         if (Input.GetKeyUp("d"))
-            force.x -= 1;
-        if (Input.GetKeyDown("space") && Physics.Raycast(transform.position, transform.up * -1, 1.1f, 1 << LayerMask.NameToLayer("Ground")))
-            body.AddForce(new Vector3(0, 300, 0));
-        curForce = transform.rotation * force.normalized * movForce;
+            speed.x -= 1;
+        if (Input.GetKeyDown("space"))
+            body.AddForce(new Vector3(0, 10, 0));
+        body.velocity = speed.normalized * maxSpeed;
+
         float rX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
         rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
---
+++ Assets/Scripts/TextScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<Text>().text = "FPS: " + 1.0f / Time.deltaTime;
+    }
+}
