8b2efd0ca7c565de5231e394a8e627450b7dadf4
Yipu Wang <yipu.wang@utexas.edu>
2016-12-01 12:45:48 -0600

    added animation

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
 
@@ -7,29 +6,6 @@ public class ChangeScene : MonoBehaviour {
 
     public void nextScene(string scene)
     {
-        if(GameObject.Find("HIT")!= null && !(scene.Equals("GameOver")))
-            Destroy(GameObject.Find("HIT"));
-        if(Time.timeScale==0 && PlayerPrefs.GetInt("Pause") ==1)
-        {
-            PlayerPrefs.SetInt("Pause", 0);
-            PlayerPrefs.Save();
-            Time.timeScale=1;       
-            Debug.Log("WAITING UP"); 
-        }
         SceneManager.LoadScene(scene);
     }
-
- /*
-    void nextScene(string scene)
-    {
-        StartCoroutine(wait(scene));
-    }
-    IEnumerator wait(string scene)
-    {
-        float fadeTime = GameObject.Find("Option").GetComponent<fading>().BeginFade(1);
-        yield return new WaitForSeconds(fadeTime);
-        SceneManager.LoadScene(scene);
-
-    }
-    */
 }
--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class FPSController : MonoBehaviour {
     private Vector3 speed;
@@ -13,81 +15,155 @@ public class FPSController : MonoBehaviour {
     private float maximumX = 360F;
     private float minimumY = -60F;
     private float maximumY = 60F;
+    private float ymin = 0.0f;
     float rotationY = 0F;
     private Camera playerCam;
     private bool firstPerson = true;
     private Animation ani, fpsAni;
     public GameObject fpsArm;
+    //UI
     public Canvas canvas;
+    //private Image pauseMenu;
+    private Text hpT, maxHpT, ammoT, maxAmmoT;
+    //player stats
+    private int maxAmmo = 12;
+    private int maxHealth = 200;
+    private int hp, ammoCnt;
+
+    // firelock: prevent firing while reloading
+    AudioSource shotAudio;
+    private bool fireLock;
+    private float reloadStart;
+    public GameObject gunflame;
+    private ParticleSystem flame;
     // Use this for initialization
     void Start () {
+        List<Text> txts = new List<Text>();
+        fireLock = false;
+        //pauseMenu = canvas.GetComponentInChildren<Image>();
+        //pauseMenu.enabled = false;
+        flame = gunflame.GetComponent<ParticleSystem>();
+        canvas.GetComponentsInChildren<Text>(txts);
+        for(int i = 0; i < txts.Count; i++)
+        {
+            string name = txts[i].name;
+            if (name == "ammo count")
+            {
+                ammoT = txts[i];
+                continue;
+            }
+            if (name == "max ammo")
+            {
+                maxAmmoT = txts[i];
+                continue;
+            }
+            if(name == "hp")
+            {
+                hpT = txts[i];
+                continue;
+            }
+            if(name == "max hp")
+            {
+                maxHpT = txts[i];
+            }
+        }
+
         speed = Vector3.zero;
         playerCam = GetComponentInChildren<Camera>();
         ani = GetComponent<Animation>();
         fpsAni = fpsArm.GetComponent<Animation>();
+        shotAudio = GetComponent<AudioSource>();
         Debug.Log("Clip count is " + fpsAni.GetClipCount());
         //Debug.Log(fpsAni.GetClip("fireWeapon"));
         ani["walk"].wrapMode = WrapMode.Loop;
         ani["jump"].speed = 7;
         ani["Sidewalk"].speed = 2;
         ani["Sidewalk"].wrapMode = WrapMode.Loop;
-        ani["Crouch"].speed = 3;
+        ani["Crouch"].speed = 4;
+
+        hp = maxHealth;
+        ammoCnt = maxAmmo;
+        maxHpT.text = "" + maxHealth;
+        maxAmmoT.text = "" + maxAmmo;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        hpT.text = "" + hp;
+        ammoT.text = "" + ammoCnt;
+
         if (Input.GetKeyDown("w"))
         {
+            speed.z = 0;
             ani["walk"].speed = 5;
             ani.Play("walk");
             speed.z += 22;
         }
            
-        if (Input.GetKeyUp("w")) { 
+        if (Input.GetKeyUp("w")) {
+            speed.z = 0;
             ani.Stop("walk");
-            speed.z -= 22;
         }
         if (Input.GetKeyDown("s"))
         {
+            speed.z = 0;
             ani["walk"].speed = -5;
             ani.Play("walk");
             speed.z -= 20;
         }
         if (Input.GetKeyUp("s"))
         {
+            speed.z = 0;
             ani.Stop("walk");
-            speed.z += 20;
         }
         
         if (Input.GetKeyDown("a"))
         {
+            speed.x = 0;
             ani.Play("Sidewalk");
             speed.x -= 15;
         }
         if (Input.GetKeyUp("a"))
         {
+            speed.x = 0;
             ani.Stop("Sidewalk");
-            speed.x += 15;
         }
 
         if (Input.GetKeyDown("d"))
         {
+            speed.x = 0;
             ani.Play("Sidewalk");
             speed.x += 15;
         }
         if (Input.GetKeyUp("d"))
         {
+            speed.x = 0;
             ani.Stop("Sidewalk");
-            speed.x -= 15;
+        }
+        if (Input.GetKeyDown("left ctrl"))
+        {
+            ani.Play("Crouch");
+            ymin = -.6f;
+        }
+        if (Input.GetKeyUp("left ctrl"))
+        {
+            ani.Play("Stand");
+            ymin = 0.0f;
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            reload();
         }
         transform.Translate(speed * Time.deltaTime,Space.Self);
         rotate();
         if (Input.GetMouseButtonDown(0))
         {
-            fpsAni.Play("fireWeapon");
-
+            fire();
         }
-
+        if (ammoCnt == 0)
+        {
+            reload();
+        }
         // buggy third person view
         if (Input.GetKeyDown("f"))
             thirdPersonView();
@@ -96,16 +172,42 @@ public class FPSController : MonoBehaviour {
             jumpSpeed.y = 0.6f;
             ani.Play("jump");
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+       }
+    private void fire()
+    {
+        if (fpsAni.isPlaying)
+            return;
+        fpsAni.Play("fireWeapon");
+        ammoCnt -= 1;
+        flame.Emit(1);
+        shotAudio.Play();
     }
+    private void reload()
+    {
+        if (ammoCnt == maxAmmo)
+            return;
+        if (fpsAni.isPlaying)
+            return;
+        reloadStart = Time.time;
+        fpsAni.Play("Reload");
+        
+        ammoCnt = maxAmmo;
+    }
+
+
 
     void FixedUpdate()
     {
-        if (transform.position.y == 0.0f && jumpSpeed.y < 0.1f) return;
-
+        
+        if (transform.position.y == ymin && jumpSpeed.y < 0.1f) return;
+  
         jumpSpeed.y -= gravity * Time.fixedDeltaTime;
         transform.Translate(jumpSpeed);
-        if (transform.position.y < 0.0f)
-            transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
+        if (transform.position.y < ymin)
+            transform.position = new Vector3(transform.position.x, ymin, transform.position.z);
     }
     private void thirdPersonView()
     {
@@ -132,6 +234,6 @@ public class FPSController : MonoBehaviour {
 
         playerCam.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
         transform.localEulerAngles = new Vector3(0, rotationX, 0);
-
     }
+
 }
