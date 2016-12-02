ab42dfb4852ba8f73a201f02d3fe1520087158ea
zappymax <westmax@sbcglobal.net>
2016-12-01 22:51:23 -0600

    added in basics for RTS mode

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class CameraControl : MonoBehaviour {
-    private const int zoomSpeed = 5;
-    private const int zoomMin = 25;
-    private const int zoomMax = 100;
+    private const int zoomSpeed = 40;
+    private const int zoomMin = 0;
+    private const int zoomMax = 1000;
     private const int boundary = 40;
-    private Vector3 scrollSpeed = Vector3.zero;
+    private int zoom = 400;
+    private Vector3 scrollSpeed, zoomVector = Vector3.zero;
     private int width, height;
-    public Text debugInfo;
+
     void Start()
     {
         width = Screen.width;
@@ -18,8 +19,7 @@ public class CameraControl : MonoBehaviour {
 
     void Update()
     {
-        debugInfo.text = "width: " + width + "\nheight: " + height+"\n" 
-            + Input.mousePosition;
+
 
         if (Input.mousePosition.x > width - boundary)
             scrollSpeed = new Vector3(30, 0, 0);
@@ -31,8 +31,22 @@ public class CameraControl : MonoBehaviour {
             scrollSpeed = new Vector3(0, 0, 30);
         else
             scrollSpeed = Vector3.zero;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0 && zoom <= zoomMax)
+        {
+            zoom += zoomSpeed;
+            zoomVector = new Vector3(0, zoomSpeed, 0);
+        }
+        else if (scroll < 0 && zoom >= zoomMin)
+        {
+            zoom -= zoomSpeed;
+            zoomVector = new Vector3(0, -zoomSpeed, 0);
+        }
+        else
+            zoomVector = Vector3.zero;
 
 
+        transform.Translate(zoomVector * Time.deltaTime);
         transform.Translate(scrollSpeed * Time.deltaTime);
     }
     // Use this for initialization
