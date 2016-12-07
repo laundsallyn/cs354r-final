8f48d38c23dc74bc535f5467c0fa7e985d55ea72
zappymax <westmax@sbcglobal.net>
2016-12-06 21:54:43 -0600

    added rts unit selection

---
+++ Assets/Scripts/RTSSelectionCam.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RTSSelectionCam : MonoBehaviour {
+
+    bool isSelecting = false;
+    Vector3 mousePosition1;
+
+    void Update()
+    {
+        // If we press the left mouse button, save mouse location and begin selection
+        if (Input.GetMouseButtonDown(0))
+        {
+            isSelecting = true;
+            mousePosition1 = Input.mousePosition;
+        }
+        // If we let go of the left mouse button, end selection
+        if (Input.GetMouseButtonUp(0))
+            isSelecting = false;
+    }
+
+    public bool IsWithinSelectionBounds(GameObject gameObject)
+    {
+        if (!isSelecting)
+            return false;
+
+        var camera = Camera.main;
+        var viewportBounds =
+            Utils.GetViewportBounds(camera, mousePosition1, Input.mousePosition);
+
+        return viewportBounds.Contains(
+            camera.WorldToViewportPoint(gameObject.transform.position));
+    }
+
+    void OnGUI()
+    {
+        if (isSelecting)
+        {
+            // Create a rect from both mouse positions
+            var rect = Utils.GetScreenRect(mousePosition1, Input.mousePosition);
+            Utils.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
+            Utils.DrawScreenRectBorder(rect, 2, new Color(0.8f, 0.8f, 0.95f));
+        }
+    }
+}
