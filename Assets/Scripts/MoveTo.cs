1092cee57dfda2c0cdb2026232126bd89694ac9a
zappymax <westmax@sbcglobal.net>
2016-12-06 23:40:20 -0600

    added more for rts movement

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -14,6 +14,7 @@ public class MoveTo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
 }
--- Assets/Scripts/RTSSelectionCam.cs
+++ Assets/Scripts/RTSSelectionCam.cs
@@ -4,19 +4,68 @@ using System.Collections;
 public class RTSSelectionCam : MonoBehaviour {
 
     bool isSelecting = false;
+    bool firstSelect = true;
     Vector3 mousePosition1;
+    ArrayList selectedObjects = new ArrayList();
+    GameObject[] allUnits;
+
+    void Start()
+    {
+        allUnits = GameObject.FindGameObjectsWithTag("EnemyUnit");
+    }
 
     void Update()
     {
         // If we press the left mouse button, save mouse location and begin selection
         if (Input.GetMouseButtonDown(0))
         {
+            if (firstSelect)
+            {
+                selectedObjects.Clear();
+                firstSelect = false;
+            }
+            else
+            {
+                foreach (GameObject go in allUnits)
+                {
+                    if (IsWithinSelectionBounds(go) && !selectedObjects.Contains(go))
+                    {
+                        print("adding object to selected");
+                        selectedObjects.Add(go);
+                    }
+                }
+            }
             isSelecting = true;
             mousePosition1 = Input.mousePosition;
         }
         // If we let go of the left mouse button, end selection
         if (Input.GetMouseButtonUp(0))
+        {
             isSelecting = false;
+            firstSelect = true;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            //print("right mouse click");
+            RTSClick();
+        }
+    }
+
+    void RTSClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject dest = new GameObject();
+            dest.transform.position = hit.point;
+            foreach(GameObject go in selectedObjects)
+            {
+                MoveTo mt = go.GetComponent<MoveTo>();
+                mt.goal = dest.transform;
+            }
+        }
     }
 
     public bool IsWithinSelectionBounds(GameObject gameObject)
