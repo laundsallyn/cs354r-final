f56f734c172576053c0730e4d7062ead1c04e4fd
Peter Scamman <pscamman@gmail.com>
2016-12-08 18:21:55 -0600

    Adding changes from Peter

--- Assets/Scripts/RigidController.cs
+++ Assets/Scripts/RigidController.cs
@@ -5,17 +5,27 @@ using System.Collections.Generic;
 
 public class RigidController : NetworkBehaviour
 {
-    private int damage = 45;
-    private Vector3 force = Vector3.zero;
-    private Vector3 curForce = Vector3.zero;
-    private float movForce = 200;
-    private float maxVel = 5;
+    private class WASD
+    {
+        public bool w = false;
+        public bool a = false;
+        public bool s = false;
+        public bool d = false;
+
+        public Vector3 GetV3()
+        {
+            return new Vector3(System.Convert.ToInt32(d) - System.Convert.ToInt32(a),
+                               0,
+                               System.Convert.ToInt32(w) - System.Convert.ToInt32(s));
+        }
+    }
+    private WASD    wasd = new WASD();
+    private float speed = 5;
     private float sensitivityX = 1F;
     private float sensitivityY = 1F;
     private float minimumY = -60F;
     private float maximumY = 60F;
-    private ConstantForce constForce;
-    //private GameObject bulletHole;
+
     float rotationY = 0F;
     private Camera mainCam;
     private Camera playerCam;
@@ -37,18 +47,29 @@ public class RigidController : NetworkBehaviour
     private GameObject gunflame;
     private ParticleSystem flame;
     private bool firstPerson;
+    [SyncVar]
+    private Animation animation;
     void Start()
     {
+
+        if (isServer && isLocalPlayer)
+        {
+            Destroy(gameObject);
+            return;
+        }
         List<Text> txts = new List<Text>();
         //pauseMenu = canvas.GetComponentInChildren<Image>();
         //pauseMenu.enabled = false;
-        transf = transform.Find("MC");
-        canvas =  transform.Find("HUD").gameObject.GetComponent<Canvas>();
-        gunflame = transform.Find("MC/Camera/fire").gameObject;
-        fpsArm = transform.Find("MC/Camera/Arm").gameObject;
-        //bulletHole = transform.Find("bullet hole").gameObject;
-        flame = gunflame.GetComponent<ParticleSystem>();
-        canvas.GetComponentsInChildren<Text>(txts);
+        transf    = transform.Find("MC");
+        animation = transf.gameObject.GetComponent<Animation>();
+        canvas    = transform.Find("HUD").gameObject.GetComponent<Canvas>();
+        gunflame  = transform.Find("MC/Camera/fire").gameObject;
+        fpsArm    = transform.Find("MC/Camera/Arm").gameObject;
+        flame     = gunflame.GetComponent<ParticleSystem>();
+        canvas.GetComponentsInChildren(txts);
+        Debug.Log("" + transform.rotation + transf.localRotation);
+        transf.localRotation = transform.rotation;
+        transform.rotation = Quaternion.identity;
         for (int i = 0; i < txts.Count; i++)
         {
             string name = txts[i].name;
@@ -75,6 +96,8 @@ public class RigidController : NetworkBehaviour
         ani = transform.Find("MC").gameObject.GetComponent<Animation>();
         fpsAni = fpsArm.GetComponent<Animation>();
         shotAudio = GetComponent<AudioSource>();
+        //Debug.Log("Clip count is " + fpsAni.GetClipCount());
+        //Debug.Log(fpsAni.GetClip("fireWeapon"));
         ani["walk"].wrapMode = WrapMode.Loop;
         ani["jump"].speed = 7;
         ani["Sidewalk"].speed = 2;
@@ -85,20 +108,23 @@ public class RigidController : NetworkBehaviour
         ammoCnt = maxAmmo;
         maxHpT.text = "" + maxHealth;
         maxAmmoT.text = "" + maxAmmo;
-        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        mainCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
         playerCam = GetComponentInChildren<Camera>();
         body = GetComponentInChildren<Rigidbody>();
-        constForce = GetComponentInChildren<ConstantForce>();
         if (isLocalPlayer)
         {
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
             foreach (Renderer r in GetComponentsInChildren<Renderer>())
                 r.enabled = false;
             foreach (Renderer r in fpsArm.GetComponentsInChildren<Renderer>())
                 r.enabled = true;
+            mainCam.GetComponent<AudioListener>().enabled = false;
             mainCam.enabled = false;
         }
         else
         {
+            canvas.enabled = false;
+
             foreach (Renderer r in fpsArm.GetComponentsInChildren<Renderer>())
                 r.enabled = false;
 
@@ -108,14 +134,20 @@ public class RigidController : NetworkBehaviour
 
     void OnDestroy()
     {
-        mainCam.enabled = true;
+        GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+        Camera mc = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        mc.enabled = true;
+        mc.GetComponent<AudioListener>().enabled = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (!isLocalPlayer) return;
-        body.AddForce(curForce - (new Vector3(System.Math.Abs(body.velocity.x) * body.velocity.x, 0, System.Math.Abs(body.velocity.z) * body.velocity.z)));
+
+        Vector3 move = body.rotation * wasd.GetV3().normalized * speed;
+        Vector3 v    = body.velocity; v.y = 0;
+        body.AddForce(move - v, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
@@ -123,52 +155,28 @@ public class RigidController : NetworkBehaviour
     {
         if (!isLocalPlayer) return;
 
-        if (Input.GetKeyDown("w"))
-        {
-            ani["walk"].speed = 5;
-            ani.Play("walk");
-            force.z += 1;
-        }
-        if (Input.GetKeyUp("w"))
-        {
-            force.z -= 1;
-            ani.Stop("walk");
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            ani["walk"].speed = -5;
-            ani.Play("walk");
-            force.z -= 1;
-        }
-        if (Input.GetKeyUp("s"))
-        {
-            ani.Stop("walk");
-            force.z += 1;
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            ani.Play("Sidewalk");
-            force.x -= 1;
+        wasd.w = (wasd.w || Input.GetKeyDown("w")) && !Input.GetKeyUp("w");
+        wasd.a = (wasd.a || Input.GetKeyDown("a")) && !Input.GetKeyUp("a");
+        wasd.s = (wasd.s || Input.GetKeyDown("s")) && !Input.GetKeyUp("s");
+        wasd.d = (wasd.d || Input.GetKeyDown("d")) && !Input.GetKeyUp("d");
 
-        }
-        if (Input.GetKeyUp("a"))
+        Vector3 mov = wasd.GetV3();
+        if (mov.z != 0)
         {
-            force.x += 1;
-            ani.Stop("Sidewalk");
-
+            ani["walk"    ].speed = mov.z * 5;
+            ani.Play("walk");
         }
-        if (Input.GetKeyDown("d"))
+        else
+        if (mov.x != 0)
         {
+            ani["Sidewalk"].speed = mov.x * 5;
             ani.Play("Sidewalk");
-            force.x += 1;
         }
-        if (Input.GetKeyUp("d"))
+        else
         {
+            ani.Stop("walk");
             ani.Stop("Sidewalk");
-            force.x -= 1;
         }
-        if(ammoCnt == 0)
-            reload();
 
         if (Input.GetKeyDown("r"))
         {
@@ -177,16 +185,17 @@ public class RigidController : NetworkBehaviour
 
         if (Input.GetMouseButtonDown(0))
             fire();
+        if (ammoCnt == 0)
+            reload();
+
         if (Input.GetKeyDown("space") && Physics.Raycast(transf.position, transf.up * -1, 1.1f, 1 << LayerMask.NameToLayer("Ground")))
         {
             ani.Play("jump");
             body.AddForce(new Vector3(0, 300, 0));
         }
-        //if (Input.GetKeyDown("f"))
-        //    thirdPersonView();
         if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
-        curForce = transf.rotation * force.normalized * movForce;
+
         float rX = transf.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
         rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
@@ -195,6 +204,7 @@ public class RigidController : NetworkBehaviour
         playerCam.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
         transf.localEulerAngles = new Vector3(0, rX, 0);
     }
+
     private void fire()
     {
         if (fpsAni.isPlaying)
@@ -202,21 +212,7 @@ public class RigidController : NetworkBehaviour
         fpsAni.Play("fireWeapon");
         ammoCnt -= 1;
         flame.Emit(1);
-        Debug.Log(gunflame.name);
         shotAudio.Play();
-        Vector3 direction = playerCam.transform.TransformDirection(Vector3.forward);
-        RaycastHit hit ;
-        if (Physics.Raycast(playerCam.transform.position, direction, out hit, 300))
-        {
-            Vector4 dmgInfo = new Vector4();
-            dmgInfo.x = hit.normal.x;
-            dmgInfo.y = hit.normal.y;
-            dmgInfo.z = hit.normal.z;
-            dmgInfo.w = damage;
-            Debug.DrawLine(playerCam.transform.position, hit.point, Color.cyan);
-            hit.collider.SendMessageUpwards("applyDamage", dmgInfo, SendMessageOptions.DontRequireReceiver);
-            
-        }
     }
     private void reload()
     {
@@ -228,21 +224,26 @@ public class RigidController : NetworkBehaviour
 
         ammoCnt = maxAmmo;
     }
-    public virtual void applyDamage(Vector4 dmg)
+
+    private void thirdPersonView()
     {
-        if (hp <= 0)
-            return;
-        hp -= (int)dmg.w;
-        Debug.Log("current hp is " + hp);
-        Vector3 dir = new Vector3(-dmg.x, -dmg.y, -dmg.z);
-        if (hp < 0)
-            dead(dir);
-    }
-    public virtual void dead(Vector3 direction)
-    {
-        body.mass = 0.2f;
-        body.constraints = RigidbodyConstraints.None;
-        Destroy(this, 5);
-        body.AddRelativeForce(direction);
+        if (firstPerson)
+        {
+            firstPerson = false;
+            playerCam.transform.Translate(new Vector3(1, 0, -19), Space.Self);
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+                r.enabled = true;
+            foreach (Renderer r in fpsArm.GetComponentsInChildren<Renderer>())
+                r.enabled = false;
+        }
+        else
+        {
+            playerCam.transform.Translate(new Vector3(-1, 0, 19), Space.Self);
+            firstPerson = true;
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+                r.enabled = false;
+            foreach (Renderer r in fpsArm.GetComponentsInChildren<Renderer>())
+                r.enabled = true;
+        }
     }
 }
